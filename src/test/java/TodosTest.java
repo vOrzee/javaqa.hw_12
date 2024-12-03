cd9042efa5ff2ef36b17cd6c69f2fb39224a10f7
Roman Leshin <vorzee@gmail.com>
2024-12-04 04:39:43 +0500

    Добавили классы согласно предусловию задания

---
+++ src/test/java/TodosTest.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.tasks.Epic;
+import ru.netology.tasks.Meeting;
+import ru.netology.tasks.SimpleTask;
+import ru.netology.tasks.Task;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodosTest {
+
+    @Test
+    public void shouldAddThreeTasksOfDifferentType() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = { simpleTask, epic, meeting };
+        Task[] actual = todos.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
