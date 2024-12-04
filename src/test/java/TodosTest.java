2e26106d7721a187895eb75161277713b8d28b99
Roman Leshin <vorzee@gmail.com>
2024-12-04 06:24:12 +0500

    Добавлены тесты для метода search

--- src/test/java/TodosTest.java
+++ src/test/java/TodosTest.java
@@ -10,28 +10,84 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TodosTest {
 
-    @Test
-    public void shouldAddThreeTasksOfDifferentType() {
-        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+    SimpleTask simpleTask;
+    Epic  epic;
+    Meeting meeting;
+    Todos todos;
+
+    @BeforeEach
+    void setUp() {
+        simpleTask = new SimpleTask(5, "Позвонить друзьям, сказать зайти в НетоБанк");
 
         String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
-        Epic epic = new Epic(55, subtasks);
+        epic = new Epic(55, subtasks);
 
-        Meeting meeting = new Meeting(
+        meeting = new Meeting(
                 555,
                 "Выкатка 3й версии приложения",
                 "Приложение НетоБанка",
                 "Во вторник после обеда"
         );
 
-        Todos todos = new Todos();
+        todos = new Todos();
+    }
 
+    @Test
+    public void shouldAddDifferentTasks() {
         todos.add(simpleTask);
         todos.add(epic);
         todos.add(meeting);
-
         Task[] expected = { simpleTask, epic, meeting };
         Task[] actual = todos.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindTaskInSimpleTask() {
+        todos.add(simpleTask);
+        Task[] expected = {simpleTask};
+        Task[] actual = todos.search("друзьям");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindTaskInEpic() {
+        todos.add(epic);
+        Task[] expected = {epic};
+        Task[] actual = todos.search("Хлеб");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindTaskInMeeting() {
+        todos.add(meeting);
+        Task[] expected = {meeting};
+        Task[] actual = todos.search("приложения");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnEmptyArray() {
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+        Task[] expected = {};
+        Task[] actual = todos.search("Пицца");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindManyTasks() {
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+        Task[] expected = {simpleTask, meeting};
+        Task[] actual = todos.search("НетоБанк");
+
         Assertions.assertArrayEquals(expected, actual);
     }
 }
